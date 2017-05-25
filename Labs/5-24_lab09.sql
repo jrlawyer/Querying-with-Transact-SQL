6e35b8492c4416452102ddab09fd314cc796e2e9
jrlawyer <jrlawyer@gmail.com>
2017-05-25 08:45:31 -0400

    Add alternative ways of inserting and deleting data to lab 9

--- Labs/5-24_lab09.sql
+++ Labs/5-24_lab09.sql
@@ -2,7 +2,7 @@
 
 --Challenge 1: Inserting products
 
---1. Insert a product
+--1. Insert a product, using nulls and defaults
 INSERT INTO SalesLT.Product
 VALUES
 ('LED Lights', 'LT-L123', NULL, 2.56, 12.99, NULL, NULL, 37, NULL, GETDATE(), NULL, NULL, NULL, NULL, DEFAULT, DEFAULT);
@@ -14,8 +14,16 @@ SELECT SCOPE_IDENTITY();
 SELECT * FROM SalesLT.Product
 WHERE ProductID = '1001';
 
+--Alternative: Insert a product by specifying the columns
+INSERT INTO SalesLT.Product(Name, ProductNumber, StandardCost, ListPrice, ProductCategoryID, SellStartDate)
+VALUES
+('Fancy LED Lights', 'LT-L124', 2.56, 12.99, 37, GETDATE());
+
+SELECT * FROM SalesLT.Product
+WHERE ProductID = 1005;
+
 --2. Insert a new category with two products
---New category
+--New category, using defaults
 INSERT INTO SalesLT.ProductCategory
 VALUES
 (4, 'Bells and Horns', DEFAULT, DEFAULT);
@@ -27,7 +35,7 @@ SELECT SCOPE_IDENTITY();
 SELECT * FROM SalesLT.ProductCategory
 WHERE ProductCategoryID = 42;
 
---New products
+--New products, using nulls and defaults
 INSERT INTO SalesLT.Product
 VALUES
 ('Bicycle Bell', 'BB-RING', NULL, 2.47, 4.99, NULL, NULL, 42, NULL, GETDATE(), NULL, NULL, NULL, NULL, DEFAULT, DEFAULT),
@@ -36,6 +44,24 @@ VALUES
 --View the new rows
 SELECT * FROM SalesLT.Product
 WHERE ProductCategoryID = 42;
+
+--Alternative:  Inserting new category and products by specifying the columns
+INSERT INTO SalesLT.ProductCategory(ParentProductCategoryID, Name)
+VALUES
+(4, 'Bells and Horns');
+
+SELECT SCOPE_IDENTITY();
+
+SELECT * FROM SalesLT.ProductCategory
+WHERE ProductCategoryID = 43;
+
+INSERT INTO SalesLT.Product(Name, ProductNumber, StandardCost, ListPrice, ProductCategoryID, SellStartDate)
+VALUES
+('Bicycle Bell', 'BB-RING', 2.47, 4.99, 43, GETDATE()),
+('Bicycle Horn', 'BB-PARP', 1.29, 3.75, IDENT_CURRENT('SalesLT.ProductCategory'), GETDATE());
+
+SELECT * FROM SalesLT.Product
+WHERE ProductCategoryID = 43;
 
 --Challenge 2:  Updating Products
 
@@ -89,3 +115,28 @@ WHERE ProductCategoryID = 42;
 
 DELETE FROM SalesLT.ProductCategory
 WHERE ProductCategoryID = 42;
+
+--Deleting products and category using subquery
+
+DELETE 
+FROM SalesLT.Product
+WHERE ProductCategoryID = 
+	(SELECT	ProductCategoryID
+	FROM SalesLT.ProductCategory
+	WHERE Name = 'Bells and Horns');
+
+SELECT * 
+FROM SalesLT.Product 
+WHERE ProductCategoryID = '43';
+
+DELETE
+FROM SalesLT.ProductCategory
+WHERE ProductCategoryID =
+	(SELECT ProductCategoryID
+	FROM SalesLT.ProductCategory
+	WHERE Name = 'Bells and Horns');
+
+SELECT * 
+FROM SalesLT.ProductCategory 
+WHERE ProductCategoryID = '43';
+
